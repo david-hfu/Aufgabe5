5dad878375828eda8204b5f272f0bcaf2c8eea10
pfaudavi <david.pfau@hs-furtwangen.de>
2022-04-05 15:26:41 +0200

    Praktikum6

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -1,10 +1,23 @@
 package de.hfu;
 import java.util.Scanner;
 
-
+/*
+ * Die App Klasse in der alles drinnen ist.
+ * Sie ist sehr toll.
+ */
 public class App {
+	/*
+	 * Nun folgt die Main Klasse die den ganzen Code beinhaltet:
+	 * Es sind nur 2 Zeilen Code
+	 */
     public static void main( String[] args ) {
+    	/*
+    	 * Nun wird erst der Input gelesen
+    	 * und dann als Grossbuchstabe ausgegeben.
+    	 */
+    	System.out.println("Gib ein den Spass:");
     	Scanner scanner = new Scanner(System.in);
         System.out.println(scanner.nextLine().toUpperCase());
+        //scanner.close();
     }
 }
--- src/test/java/de/hfu/AppTest.java
+++ src/test/java/de/hfu/AppTest.java
@@ -17,4 +17,28 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    @Test
+    public void utilTest() {
+    	assert(Util.istErstesHalbjahr(1));
+    }
+    
+    @Test (expected = IllegalStateException.class)
+    public void queueLeerTest( ) {
+    	Queue qEins = new Queue(3);
+    	qEins.dequeue();
+    }
+    
+    @Test 
+    public void queueTest( ) {
+    	Queue q = new Queue(3);
+    	q.enqueue(1);
+    	q.enqueue(2);
+    	q.enqueue(3);
+    	q.enqueue(4);
+    	assert(q.dequeue() == 1);
+    	assert(q.dequeue() == 2);
+    	assert(q.dequeue() == 4);
+    }
+    
 }
