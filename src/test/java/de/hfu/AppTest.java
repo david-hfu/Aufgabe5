b09279e103673d77acffdecdf3053f199dd42432
pfaudavi <david.pfau@hs-furtwangen.de>
2022-04-14 00:00:18 +0200

    Praktikum 7 Schritt 4 Tests geschrieben

--- src/test/java/de/hfu/AppTest.java
+++ src/test/java/de/hfu/AppTest.java
@@ -2,7 +2,17 @@ package de.hfu;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+
 import org.junit.Test;
+
+import de.hfu.residents.domain.Resident;
+import de.hfu.residents.repository.*;
+import de.hfu.residents.service.BaseResidentService;
+import de.hfu.residents.service.ResidentService;
+import de.hfu.residents.service.ResidentServiceException;
 
 /**
  * Unit test for simple App.
@@ -40,5 +50,58 @@ public class AppTest
     	assert(q.dequeue() == 2);
     	assert(q.dequeue() == 4);
     }
+    
+    
+    class Residents implements ResidentRepository {
+
+		@Override
+		public List<Resident> getResidents() {
+			// TODO Auto-generated method stub
+			Vector<Resident> testResident = new Vector<Resident>();
+			testResident.add(new Resident("David", "Pfau", "Rankweg", "Freudenstadt", new Date()));
+			testResident.add(new Resident("Lukas", "Lokomotivfuehrer", "Lokstrasse", "Insel", new Date()));
+			testResident.add(new Resident("Jim", "Knopf", "Lokstrasse", "Insel", new Date()));
+			testResident.add(new Resident("Jimmy", "Knopf", "Robert Gerwig Platz", "Furtwangen", new Date()));
+			testResident.add(new Resident("Can", "Demir", "Am Grosshausberg", "Furtwangen", new Date()));
+			testResident.add(new Resident("Yannic", "Weinert", "Sportplatz", "Hotzenwald", new Date()));
+			testResident.add(new Resident("Dominik", "Sprinzl", "Zahnriemenstrasse", "Civic", new Date()));
+			testResident.add(new Resident("Linus", "Torvalds", "Kernelstrasse", "GNU/Linux", new Date()));
+			return testResident;
+		}
+    	
+    }
+    
+    @Test
+    public void filterTest() {
+    	BaseResidentService baseResidentService = new BaseResidentService();
+    	Residents residentRepository = new Residents();
+    	baseResidentService.setResidentRepository(residentRepository);
+    	assert (baseResidentService.getFilteredResidentsList(new Resident("", "Knopf", "", "", new Date())).size() == 2);
+    	assert (baseResidentService.getFilteredResidentsList(new Resident("", "Torvalds", "", "", new Date())).size() == 1);
+    	assert (baseResidentService.getFilteredResidentsList(new Resident("D*", "", "", "", new Date())).size() == 2);
+    	
+    	try {
+			assert (baseResidentService.getUniqueResident(new Resident("David", "", "", "", new Date())).getFamilyName() == "Pfau" );
+		} catch (ResidentServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			assert(false);
+		}
+    	try {
+			assert (baseResidentService.getUniqueResident(new Resident("", "Torvalds", "", "", new Date())).getStreet() == "Kernelstrasse" );
+		} catch (ResidentServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			assert(false);
+		}
+    	try {
+			baseResidentService.getUniqueResident(new Resident("", "", "", "Insel", new Date()));
+			assert(false);
+		} catch (ResidentServiceException e) {
+			// TODO Auto-generated catch block
+			assert(true);
+		}
+    }
+    
     
 }
